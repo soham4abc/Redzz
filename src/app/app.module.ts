71a574c84ea8873091334d583487653e3bfb1282
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-05-03 13:00:54 +0530

    reload bug fixed and volunteers link added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { ToastrModule } from 'ngx-toastr';
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { TablesComponent } from './pages/tables/tables.component';
-import { FormsComponent } from './pages/forms/forms.component';
-import { TypographyComponent } from './pages/typography/typography.component';
-import { MapsComponent } from './pages/maps/maps.component';
-import { NotificationsComponent } from './pages/notifications/notifications.component';
-import { Cls10Component } from './cls10/cls10.component';
-import { Cls11Component } from './cls11/cls11.component';
-import { Cls12Component } from './cls12/cls12.component';
-import { JeeComponent } from './jee/jee.component';
-import { OxygenComponent } from './oxygen/oxygen.component';
-import { BedsComponent } from './beds/beds.component';
-import { AmbulanceComponent } from './ambulance/ambulance.component';
-import { VolsComponent } from './vols/vols.component';
-import { DonateComponent } from './donate/donate.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { TablesComponent } from "./pages/tables/tables.component";
+import { FormsComponent } from "./pages/forms/forms.component";
+import { TypographyComponent } from "./pages/typography/typography.component";
+import { MapsComponent } from "./pages/maps/maps.component";
+import { NotificationsComponent } from "./pages/notifications/notifications.component";
+import { Cls10Component } from "./cls10/cls10.component";
+import { Cls11Component } from "./cls11/cls11.component";
+import { Cls12Component } from "./cls12/cls12.component";
+import { JeeComponent } from "./jee/jee.component";
+import { OxygenComponent } from "./oxygen/oxygen.component";
+import { BedsComponent } from "./beds/beds.component";
+import { AmbulanceComponent } from "./ambulance/ambulance.component";
+import { VolsComponent } from "./vols/vols.component";
+import { DonateComponent } from "./donate/donate.component";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -48,7 +49,7 @@ import { DonateComponent } from './donate/donate.component';
     BedsComponent,
     AmbulanceComponent,
     VolsComponent,
-    DonateComponent
+    DonateComponent,
   ],
   imports: [
     BrowserModule,
@@ -56,9 +57,9 @@ import { DonateComponent } from './donate/donate.component';
     CommonModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    ToastrModule.forRoot()
-    ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ToastrModule.forRoot(),
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
